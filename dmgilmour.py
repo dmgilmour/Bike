aebe7b459ede84b6006b5963f08f56086d931833
dmgilmour <dmgilmour@pitt.edu>
2017-08-19 14:40:40 -0400

    Header uses Jinja and is easily extensible

--- dmgilmour.py
+++ dmgilmour.py
@@ -2,8 +2,20 @@ from flask import Flask, request, abort, url_for, redirect, session, render_temp
 
 app = Flask(__name__)
 
+pages = ["about", "projects", "readings", "blog", "employers"]
+
 @app.route("/")
 def default():
 
-    return render_template("home.html")
+    return render_template("head.html", pages=pages)
 
+
+
+@app.route("/<dead_end_page>/")
+def dead_end(dead_end_page):
+
+    if dead_end_page not in pages:
+        return render_template("not_found.html")
+
+    print(dead_end_page)
+    return render_template("head.html", pages=pages, cur_page=dead_end_page)

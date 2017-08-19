36813925229e295a1ddc7b2291afe8f2e793159c
dmgilmour <dmgilmour@pitt.edu>
2017-08-19 14:58:32 -0400

    First tracking attempt

--- dmgilmour.py
+++ dmgilmour.py
@@ -6,7 +6,7 @@ pages = ["about", "projects", "readings", "blog", "employers"]
 
 @app.route("/")
 def default():
-
+    log_client_info()
     return render_template("head.html", pages=pages)
 
 
@@ -14,8 +14,15 @@ def default():
 @app.route("/<dead_end_page>/")
 def dead_end(dead_end_page):
 
+    log_client_info()
+
     if dead_end_page not in pages:
         return render_template("not_found.html")
 
     print(dead_end_page)
     return render_template("head.html", pages=pages, cur_page=dead_end_page)
+
+
+def log_client_info():
+    print(request.remote_addr)
+    print(request.environ['REMOTE_ADDR'])

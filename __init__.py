8ed8d218cb99cf78bb2b3205db01e285c43f5106
Dan <dmgilmour@pitt.edu>
2019-01-20 01:01:59 +0800

    initial track commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+''' flask app with mongo '''
+
+import os
+import json
+import datetime
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+

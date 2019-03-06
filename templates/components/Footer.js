f7616b43e59947f5b8bb8c7b980747f0a5935a75
Dan <dmgilmour@pitt.edu>
2019-03-06 10:29:49 -0500

    with reqs

---
+++ templates/components/Footer.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+function Footer() {
+  return (
+    <p>This here is a header</p>
+  )
+}
+
+export default Footer
---
+++ templates/components/Header.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+function MainContent() {
+  return (
+    <p>This here is the main content</p>
+  )
+}
+
+export default MainContent

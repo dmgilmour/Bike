792240ad26e81fbbf4e2dc89dfc7eb77f927191a
Dan <dmgilmour@pitt.edu>
2019-03-20 10:14:47 -0700

    TODO DYLAN

--- dmgilmour.py
+++ dmgilmour.py
@@ -6,6 +6,9 @@ import json
 import sys
 import time
 import datetime
+import bcrypt
+from getpass import getpass
+from appAlgoTime import Algo
 from flask import Flask, request, abort, url_for, redirect, session, render_template, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,6 +16,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+algo = Algo()
+master_secret_key = getpass('Senor Design')
 
 ip_list = []
 
@@ -35,14 +40,16 @@ class Bike(db.Model):
         self.ip = ip
 """
 
-"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ip = db.column(db.String(30))
+    username = db.column(db.String(40))
+    password = db.column(db.String(70))
+    salt = db.column(db.String(40))
 
-    def __init__(self, ip):
-        self.ip = ip
-        """
+    def __init__(self, username, password, salt):
+        self.username = username
+        self.password = password
+        self.salt = salt
 
 
 class Location(db.Model):
@@ -86,17 +93,76 @@ def initdb_command():
 """
 
 
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    message = ""
+
+    if (logged_in()):
+        return redirect(url_for("home"))
+    
+    if request.method == "POST":
+        username = request.form["user"]
+        l = User.query.all()
+        a = [b for b in l if b.username == username]
+        print(a)
+        if len(a) != 0:
+            user = a[0]
+        # TODO DYLAN: replace the above line with a query that sets `user = a user going by username`
+            salt = user.salt # TODO DYLAN lookup the salt for the corresponding user
+            combopass = (request.form["pass"] + salt + master_secret_key).encode('utf-8')
+            password = bcrypt.hashpw(combopass, salt)
+            if password == user.password: # TODO DYLAN change this to check the passwor
+                session["user"] = username
+                return redirect(url_for("home"))
+            else:
+                message = "Invalid credentials"
+        else:
+            message = "Invalid credentials"
+
+    return render_template("login.html", message=message)
+
+@app.route("/signup/", methods = ["GET", "POST"])
+def signup():
+    message = ""
+
+    if (logged_in()):
+        return redirect(url_for("home"))
+
+    if request.method == "POST":
+        username = request.form["user"].encode('utf-8')
+        print(username)
+        # TODO DYlAN replace everything after 'not' with a check if username in database
+        print(User.query.filter_by(username=request.form["user"]).all())
+        if len(a) == 0:
+            salt = bcrypt.gensalt().encode('utf-8')
+            combopass = (request.form["pass"] + salt + master_secret_key).encode('utf-8')
+            password = bcrypt.hashpw(combopass, salt)
+            # TODO add a new user with username username, password password, and salt salt, you may need to add salt as a column but it is necessary. after than get rid of the next two lines
+            db.session.add(User(username, password, salt))
+            db.session.commit()
+            print(db.session.query.filter_by(username=username).first())
+            message = "New user added"
+        else:
+            message = "Username already in use"
+
+    return render_template("signup.html", message=message)
+
 @app.route("/", methods = ["GET", "POST"])
 def home():
-    
-    #time_boundary = datetime.datetime.now() - datetime.timedelta(minutes=1)
-    #l = Location.query.filter(Location.time >= time_boundary)
-    l = Location.query.all()
-    loc_list = [[i.lat, i.lon] for i in l]
-    print(loc_list)
 
-    return render_template("home.html")
-    # return render_template("home.html", bike_loc=bike_loc)
+    if (not(logged_in())):
+        return redirect(url_for("login"))
+    else:
+    
+        #time_boundary = datetime.datetime.now() - datetime.timedelta(minutes=1)
+        #l = Location.query.filter(Location.time >= time_boundary)
+        l = Location.query.all()
+        loc_list = [[i.lat, i.lon] for i in l]
+        print(loc_list)
+
+        return render_template("home.html")
+        # return render_template("home.html", bike_loc=bike_loc)
 
 @app.route("/data", methods = ["POST"])
 def trackdata():
@@ -114,11 +180,8 @@ def userdata():
         l = Location.query.all()
         loc_list = [[i.lat, i.lon] for i in l]
         return jsonify(loc_list)
+
     else:
-
-
-
-
         data = request.get_json()
         lat = data['lat']
         lon = data['lon']
@@ -161,3 +224,14 @@ def sw():
 def swjs():
     print("visited")
     return app.send_static_file('sw.js')
+
+def logged_in():
+    if "user" in session:
+        if not User.query.filter_by(name=session["user"]).first():
+            session.clear()
+            return False 
+        else:
+            return True
+    else:
+        return False
+
